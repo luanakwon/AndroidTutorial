a8769b65dc9a34f2691a7f94fe2479de75c5618c
luanakwon <junnyboy99@gmail.com>
2022-02-18 21:41:15 -0800

    opencv camera

--- MyKotlin/app/src/main/java/com/example/mykotlin/MainActivity.kt
+++ MyKotlin/app/src/main/java/com/example/mykotlin/MainActivity.kt
@@ -5,37 +5,30 @@ import android.os.Bundle
 import android.util.Log
 import android.Manifest
 import android.content.pm.PackageManager
-import android.opengl.Visibility
 import android.view.SurfaceView
-import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import org.opencv.android.*
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.OpenCVLoader
+import org.opencv.core.Core
+import org.opencv.core.CvType
 import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
 
-class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
+class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2{
 
     companion object {
         private const val TAG = "MainActivity: "
-        private const val REQUEST_CODE_PERMISSION = 10
+        private const val REQUEST_CODE_PERMISSION = 1001
         private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     }
 
-    private var cameraBridgeViewBase: CameraBridgeViewBase? = null
-    private val baseLoaderCallback = object : BaseLoaderCallback(this){
-        override fun onManagerConnected(status: Int) {
-            when (status){
-                LoaderCallbackInterface.SUCCESS ->{
-                    Log.e(TAG, "onManagerConnected: Opencv loaded")
-                    System.loadLibrary("native-lib")
-                    cameraBridgeViewBase?.enableView()
-                }
-                else -> super.onManagerConnected(status)
-            }
-
-        }
-    }
+    private var cameraBridgeViewBase : CameraBridgeViewBase? = null
+    private var baseLoaderCallback : BaseLoaderCallback? = null
+    //private var mat : Mat = Mat()
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSION.all{
         ContextCompat.checkSelfPermission(
@@ -47,72 +40,75 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
+        // Idk but once I remove this super it gives me an error
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode == REQUEST_CODE_PERMISSION){
             if (allPermissionsGranted()){
                 Log.i(TAG, "Permissions granted")
-
             } else {
                 Toast.makeText(
                     this, "Permissions denied by the user", Toast.LENGTH_SHORT).show()
                 this.finish()
-
             }
-
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // check if permissions are granted
         if (allPermissionsGranted()){
             Log.i(TAG, "Permissions granted")
-            //cameraBridgeViewBase?.setCameraPermissionGranted doesn't exist
         } else {
             ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSION, REQUEST_CODE_PERMISSION)
+                this, REQUIRED_PERMISSION, REQUEST_CODE_PERMISSION
+            )
         }
 
-        cameraBridgeViewBase = findViewById(R.id.camView)
-        cameraBridgeViewBase?.visibility = SurfaceView.VISIBLE
-        cameraBridgeViewBase?.setCvCameraViewListener(this)
+        cameraBridgeViewBase = findViewById(R.id.myCamView)
+        cameraBridgeViewBase!!.visibility = SurfaceView.VISIBLE
+        cameraBridgeViewBase!!.setCvCameraViewListener(this)
+        baseLoaderCallback = object : BaseLoaderCallback(this){
+            override fun onManagerConnected(status: Int) {
+                when (status){
+                    SUCCESS -> cameraBridgeViewBase!!.enableView()
+                    else -> super.onManagerConnected(status)
+                }
+            }
+        }
 
+    }
 
-//        val CDD = CardDetector(mk.ndarray(mk[170,140]),mk.ndarray(mk[160,200]),0.2f)
-//        var textView = findViewById<TextView>(R.id.textView)
-//        textView.text = CDD.area.toString()
+    override fun onPause() {
+        super.onPause()
+        cameraBridgeViewBase?.disableView()
+    }
 
-//        val a = Mat.zeros(1,4,CvType.CV_8U)
-//        val b = Mat()
-//        a.put(0,0,1.0)
-//        a.put(0,1,3.0)
-//        a.put(0,2,5.0)
-//        a.put(0,3,7.0)
-//        Core.repeat(a,3,1,b)
-//        println(a)
-//        println(b)
-//        Core.subtract(b, Scalar(2.0), b)
-//        for(i in 0..2){
-//            for(j in 0..3){
-//                println("$i, $j, ${b.get(i,j)[0]}")
-//            }
-//        }
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraBridgeViewBase?.disableView()
+    }
 
-
-
+    override fun onResume() {
+        super.onResume()
+        if(!OpenCVLoader.initDebug()){
+            Log.e(TAG, "opencv init debug error")
+        } else {
+            baseLoaderCallback?.onManagerConnected(BaseLoaderCallback.SUCCESS)
+        }
     }
 
     override fun onCameraViewStarted(width: Int, height: Int) {
-        TODO("Not yet implemented")
+        //mat = Mat(width, height, CvType.CV_8UC4)
     }
 
     override fun onCameraViewStopped() {
-        TODO("Not yet implemented")
+        //TODO "Not yet implemented"
     }
 
     override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame?): Mat {
-        TODO("Not yet implemented")
-    }
+        val mRGBA = inputFrame?.gray()
 
+        return mRGBA ?: Mat()
+    }
 }
