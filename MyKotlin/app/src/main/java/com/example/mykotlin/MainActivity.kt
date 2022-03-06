677c97f1455756684372016062264f3300f8bba5
luanakwon <junnyboy99@gmail.com>
2022-03-06 17:01:45 +0900

    camera2 + cardDetection on progress

--- MyKotlin/app/src/main/java/com/example/mykotlin/MainActivity.kt
+++ MyKotlin/app/src/main/java/com/example/mykotlin/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.media.ImageReader
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.SurfaceView
+import android.view.TextureView
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -26,7 +37,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
         private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     }
 
-    private var cameraBridgeViewBase : CameraBridgeViewBase? = null
+    // opencv baseLoaderCallback..?
     private var baseLoaderCallback : BaseLoaderCallback? = object : BaseLoaderCallback(this){
         override fun onManagerConnected(status: Int) {
             when (status){
@@ -40,8 +51,21 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
         }
     }
 
-    private var cornersDst : Array<Point>? = null
-    private var cardDetector: CardDetector? = null
+    private lateinit var cornersDst : Array<Point>
+    private lateinit var cardDetector: CardDetector
+    private lateinit var textureView: TextureView
+    private lateinit var backgroundHandlerThread: HandlerThread
+    private lateinit var backgroundHandler: Handler
+    private lateinit var cameraId: String
+    private lateinit var cameraManager: CameraManager
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
+    private lateinit var cameraCaptureSession: CameraCaptureSession
+    private lateinit var imageReader: ImageReader
+    private lateinit var previewSize: Size
+
+    private var captureSessionOccupied: Boolean = false
+    private var connectCameraFirstCall: Boolean = true
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSION.all{
         ContextCompat.checkSelfPermission(
@@ -68,6 +92,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
         // check if permissions are granted
         if (allPermissionsGranted()){
@@ -79,35 +104,41 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
             )
         }
 
+        // init opencv
         OpenCVLoader.initDebug()
 
-        // create camera surface
-        cameraBridgeViewBase = findViewById(R.id.myCamView)
-        cameraBridgeViewBase!!.visibility = SurfaceView.VISIBLE
-        cameraBridgeViewBase!!.setCvCameraViewListener(this)
+        // lateinit views
+        textureView = findViewById(R.id.textureView)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
+        // capture btn onClickListener
+        findViewById<Button>(R.id.captureBtn).setOnClickListener {captureSurface()}
+
+        // start cameraVideoThread
+        startBackgroundThread()
 
         // initialize card detector
-        Log.i(TAG, "initialize card detector")
         cardDetector = CardDetector(
             mk.ndarray(mk[170,140]), mk.ndarray(mk[160,200]),0.2f)
-        Log.i(TAG, "initialized card detector")
 
         // array of point to get detected corners of card
-        cornersDst = Array //TODO
+        cornersDst = Array<Point>(4){ Point(0.0,0.0) } //TODO -> done..?
     }
 
     override fun onPause() {
         super.onPause()
-        cameraBridgeViewBase?.disableView()
+        Log.i(TAG, "onPause called")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        cameraBridgeViewBase?.disableView()
+        Log.i(TAG, "onDestroy called")
+        cameraDevice.close()
     }
 
+    @SuppressLint("MissingPermission")
     override fun onResume() {
+        //TODO From here https://github.com/luanakwon/AndroidTutorial/blob/main/Camera2Kotlin/app/src/main/java/com/mediapipe/camera2kotlin/MainActivity.kt
         super.onResume()
         if(!OpenCVLoader.initDebug()){
             Log.e(TAG, "opencv init debug error")
