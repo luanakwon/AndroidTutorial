fbcacf697115bee3d80eeeebe8575f90e84d7975
luanakwon <junnyboy99@gmail.com>
2022-05-07 17:41:29 +0900

    tutorial video fixed, added replay button

--- RingGauge/app/src/main/java/com/Onbleep/ringgauge/HowToActivity.kt
+++ RingGauge/app/src/main/java/com/Onbleep/ringgauge/HowToActivity.kt
@@ -3,7 +3,11 @@ package com.onbleep.ringgauge
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import android.widget.VideoView
 
@@ -13,14 +17,32 @@ class HowToActivity : AppCompatActivity() {
         setContentView(R.layout.activity_how_to)
 
         val v: VideoView = findViewById(R.id.videoView)
+        val skipButton: Button = findViewById(R.id.skipBtn)
+        val replayButton: ImageButton = findViewById(R.id.replayBtn)
+        val changeTextHandler = Handler(Looper.getMainLooper())
+        val videoLengthInMillis: Long = 30000 // 30s
         // TODO: change video
-        v.setVideoPath("android.resource://com.Onbleep.ringgauge/"+R.raw.testvid)
+        v.setVideoPath("android.resource://${packageName}/${R.raw.how_to_0507}")
         v.start()
 
-        findViewById<Button>(R.id.skipBtn).setOnClickListener {
+
+        skipButton.setOnClickListener {
             val myIntent = Intent(this, InformPermissionActivity::class.java)
             startActivity(myIntent)
         }
+        replayButton.setOnClickListener {
+            v.start() // replay video
+            skipButton.setText("Skip") // change "next" to "skip"
+            changeTextHandler.postDelayed({ // set timer
+                skipButton.setText("Next") // to change the skipButton text
+                replayButton.visibility = View.VISIBLE // and to show replayButton
+            },videoLengthInMillis)
+            it.visibility = View.GONE // remove replayButton
+        }
+        changeTextHandler.postDelayed({// set timer
+            skipButton.setText("Next")
+            replayButton.visibility = View.VISIBLE
+        },videoLengthInMillis)
     }
 
     var mBackWait: Long = 0
